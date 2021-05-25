6a85871fa8a29ed2e976d97e3fefca7e52a9a250
Natália <natalia.setubal@outlook.com>
2021-05-25 00:44:27 -0300

    atualizacoes e adicao de novos pokemons

---
+++ js/acoes.js
@@ -0,0 +1,47 @@
+function Enviar1(Resposta) {
+    if( Resposta == 'Charmeleon'){
+        window.location.href ='Vitoria.html'
+    } else{
+        window.location.href ='Derrota.html'
+    }
+}
+
+function Enviar2(Resposta) {
+    if( Resposta == 'Wartortle'){
+        window.location.href ='Vitoria.html'
+    } else{
+        window.location.href ='Derrota.html'
+    }
+}
+
+function Enviar3(Resposta) {
+    if( Resposta == 'Raichu'){
+        window.location.href ='Vitoria.html'
+    } else{
+        window.location.href ='Derrota.html'
+    }
+}
+
+function Enviar4(Resposta) {
+    if( Resposta == 'Ivysaur'){
+        window.location.href ='Vitoria.html'
+    } else{
+        window.location.href ='Derrota.html'
+    }
+}
+
+function Enviar5(Resposta) {
+    if( Resposta == 'Magneton'){
+        window.location.href ='Vitoria.html'
+    } else{
+        window.location.href ='Derrota.html'
+    }
+}
+
+function Enviar6(Resposta) {
+    if( Resposta == 'Haunter'){
+        window.location.href ='Vitoria.html'
+    } else{
+        window.location.href ='Derrota.html'
+    }
+}
